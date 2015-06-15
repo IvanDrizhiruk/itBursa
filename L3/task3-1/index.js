f220c9347aefcd74d13370dd3c9f42465778ea36
IvanDrizhiruk <ivan.drizhiruk@gmail.com>
2015-06-15 22:35:52 +0300

    Гзвфеу 3.1, 3.2

--- L3/task3-1/index.js
+++ L3/task3-1/index.js
@@ -1,17 +1,15 @@
 function jsonpCallback(data) {
   'use strict';
 
-  console.log(data);
-
   var contentElement = document.querySelector('#content');
+  var text;
 
   if (data.parse) {
-    var text = data.parse.text['*'];
+    text = data.parse.text['*'];
     contentElement.innerHTML = text.replace(/href="\/wiki/g, 'href="http://en.wikipedia.org/wiki');
   } else {
-    contentElement.innerHTML = "Cannot find the knowledge";
+    contentElement.innerHTML = 'Cannot find the knowledge';
   }
-
 }
 
 (function init() {
@@ -19,20 +17,20 @@ function jsonpCallback(data) {
 
   function sendJsonpRequest(title) {
     var scriptTag = document.createElement('script');
-    scriptTag.src = "http://en.wikipedia.org/w/api.php?action=parse&page="
+    scriptTag.src = 'http://en.wikipedia.org/w/api.php?action=parse&page='
       + encodeURIComponent(title)
-      + "&prop=text&section=0&format=json&callback=jsonpCallback";
+      + '&prop=text&section=0&format=json&callback=jsonpCallback';
 
     document.getElementsByTagName('HEAD')[0].appendChild(scriptTag);
   }
 
   function find() {
-    var title = document.querySelector('#title').value;
-    sendJsonpRequest(title);
+    var title = document.querySelector('input').value;
+    sendJsonpRequest(title.replace(' ', '_'));
   }
 
   function initialiseApp() {
-    document.querySelector('#find').addEventListener('click', find);
+    document.querySelector('button').addEventListener('click', find);
   }
 
   window.addEventListener('load', initialiseApp);
--- L3/task3-2/index.js
+++ L3/task3-2/index.js
@@ -1,33 +1,40 @@
 (function init() {
   'use strict';
 
-  function isSuccessTure(responseText) {
-      try {
-        return JSON.parse(responseText).status === 'ok';
-      } catch (e) {
-        console.log(e);
-        return false;
-      }
+  function isSuccessTrue(responseText) {
+    try {
+      return JSON.parse(responseText).status === 'ok';
+    } catch (e) {
+      return false;
+    }
+  }
+
+  function isStatus200(request) {
+    return request.status === 200;
   }
 
   function sendRequest(requestType, resultCallback) {
     var request = new XMLHttpRequest();
-    request.open(requestType, 'https://cors-test.appspot.com/test');
-    request.addEventListener('readystatechange', function () {
+    request.open(requestType, 'http://cors-test.appspot.com/test');
+    request.addEventListener('readystatechange', function onRedy() {
+      var doneResult;
+
       if (request.readyState === request.DONE) {
-        var doneResult = request.status === 200 && isSuccessTure(request.responseText);
-        resultCallback && resultCallback(doneResult);
+        doneResult = isStatus200(request)
+          && isSuccessTrue(request.responseText);
+
+        resultCallback(doneResult);
       }
     });
     request.send();
   }
 
   function generateCallback(selector) {
-    return function(result) {
+    return function generated(result) {
       var element = document.querySelector(selector);
       element.classList.add(result ? 'success' : 'error');
-      element.innerHTML = result ? 'Ok' : 'Failed';
-    }
+      element.innerHTML = result ? 'OK' : 'Failed';
+    };
   }
 
   function initApp() {
@@ -37,5 +44,4 @@
   }
 
   window.addEventListener('load', initApp);
-
 })();
