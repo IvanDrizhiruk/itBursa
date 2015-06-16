4e64767aba4d52fd4dab6c62278e58c3e0dc428a
ivan.drizhiruk <ivan.drizhiruk@gmail.com>
2015-06-16 18:00:54 +0300

    Update 3.3

--- L3/task3-3/index.js
+++ L3/task3-3/index.js
@@ -1,21 +1,26 @@
 //(function init() {
 //    'use strict';
-
-    var myId;
     var gameId;
+    var playerId;
+
+    var GAME_STATE = {
+        INITIALIZE: 'initialize',
+        IN_PROGRESS: 'inProgres',
+        ENDED: 'ended'
+    };
 
     var GAME = {
-        mySide: undefined
+        mySide: undefined,
+        nowSide: 'x',
+        status: GAME_STATE.INITIALIZE
     };
 
 
     var SEND = {
-        POST: postToServer
+        POST: postToServer,
+        GET: getToServer
     };
-
     var ELEMENTS;
-
-
     var SOCKET = {
         socket :undefined,
         send: function(obj) {
@@ -37,12 +42,21 @@
     }
 
     function showMessage(message) {
-        ELEMENTS.statusMessage.innerHTML = message
+        ELEMENTS.startGameStatusMessage.innerHTML = message
     }
 
-    function postToServer(url, resultCallback) {
+    function postToServer(url, data, headers, postResultCallback) {
         var request = new XMLHttpRequest();
         request.open('POST', url);
+        if (data) {
+            request.setRequestHeader("Content-type","application/json");
+        }
+        if(headers) {
+            for(index in headers) {
+                request.setRequestHeader(index, headers[index]);
+            }
+        }
+
         request.addEventListener('readystatechange', function () {
             if (request.readyState === request.DONE) {
                 var result;
@@ -50,11 +64,42 @@
                     result = safeJSONparse(request.responseText);
                 }
 
-                if (resultCallback) {
-                    resultCallback(result, request.status);
+                if (postResultCallback) {
+                    postResultCallback(result, request.status);
                 }
             }
         });
+
+        if (data) {
+            request.send(JSON.stringify(data));
+        } else {
+            request.send();
+        }
+    }
+
+    function getToServer(url, headers, getResultCallback) {
+        var request = new XMLHttpRequest();
+        request.open('GET', url);
+
+        if(headers) {
+            for(index in headers) {
+                request.setRequestHeader(index, headers[index]);
+            }
+        }
+
+        request.addEventListener('readystatechange', function () {
+            if (request.readyState === request.DONE) {
+                var result;
+                if (request.status === 200 && request.responseText) {
+                    result = safeJSONparse(request.responseText);
+                }
+
+                if (getResultCallback) {
+                    getResultCallback(result, request.status);
+                }
+            }
+        });
+
         request.send();
     }
 
@@ -69,29 +114,49 @@
         var li = document.createElement('li');
         li.innerHTML = 'Game_' + gameId;
         li.gameId = gameId;
-        //li.classList.add('liclass_' + gameId);
+        li.classList.add('liclass_' + gameId);
         li.addEventListener('dblclick', joinToGame);
         return li;
     }
 
     function registryGameId(id) {
-        if (myId === id) {
+        if (gameId === id) {
             return;
         }
         document.querySelector('.existing-games').appendChild(newGemeId(id));
     }
 
+    function findCellByIndex(index) {
+        return document.querySelectorAll('.cell')[index -1];
+    }
+
+    function listenOfOpponent(callback) {
+        var headers = {'Game-ID': gameId, 'Player-ID': playerId};
+        var callback = function (data, statusCode) {
+            if (200 != statusCode && GAME.status === GAME_STATE.IN_PROGRESS) {
+                SEND.GET(gameUrls.move, headers, callback);
+                return;
+            }
+
+            var element = findCellByIndex(data.move);
+            element.classList.add(GAME.opositSide);
+            GAME.nowSide = 'x' === GAME.nowSide ? 'o' : 'x'
+
+            if (data && data.win) {
+                showWinnMessage(data.win);
+                GAME.status = GAME_STATE.ENDED;
+            }
+        };
+
+        SEND.GET(gameUrls.move, headers, callback)
+    }
     function startGameWith(id) {
-        if (!myId) {
-            return;
-        }
+    playerId = id;
 
-        gameId = id;
+    ELEMENTS.createGame.disabled = true;
+    showMessage('Ожидаем начала игры');
 
-        ELEMENTS.createGame.disabled = true;
-        showMessage('Ожидаем начала игры');
-
-        SEND.POST(gameUrls.gameReady, {id: myId, gameId: gameId}, function (data, statusCode) {
+    SEND.POST(gameUrls.gameReady, {player: playerId, game: gameId}, undefined, function (data, statusCode) {
             if(statusCode === 408) {
                 showMessage('Ошибка старта игры: другой игрок не ответил');
             } else if(statusCode === 410) {
@@ -100,28 +165,45 @@
                 showMessage('Some error');
             } else {
                 GAME.mySide = data.side;
+                GAME.opositSide = 'x' === data.side ? 'o' : 'x'
 
                 ELEMENTS.startGame.style.display = 'none';
                 ELEMENTS.mainGame.style.display = 'block';
+
+                GAME.status = GAME_STATE.IN_PROGRESS;
+                if(GAME.mySide !== GAME.nowSide) {
+                    listenOfOpponent();
+                }
             }
         });
     }
 
+    function removeElement(element) {
+        if(element && element.parentElement) {
+            element.parentElement.removeChild(element);
+        }
+    }
+
+    function removeGame(id) {
+        removeElement(document.querySelector('.liclass_' + id.replace('.', '\\.')));
+    }
+
     function handleSocetMessage(message) {
         var data = JSON.parse(message.data);
-        //console.log(message);
 
         switch (data.action) {
             case 'add':
                 registryGameId(data.id);
                 break;
             case 'remove':
+                removeGame(data.id);
                 break;
             case 'startGame':
                 startGameWith(data.id);
                 break;
             default:
             {
+                alert('Some error');
                 console.log('Some error');
                 console.log(message);
             }
@@ -152,23 +234,123 @@
     function onCreateGame() {
         ELEMENTS.createGame.disabled = true;
 
-        SEND.POST(gameUrls.newGame, function (data, status) {
+        SEND.POST(gameUrls.newGame, undefined, undefined, function (data, status) {
             if (!data) {
                 showMessage('"Ошибка создания игры');
             }
 
-            myId = data.yourId;
+            gameId = data.yourId;
 
-            SOCKET.send({register: myId}) //TODO can be send error
+            SOCKET.send({register: gameId}) //TODO can be send error
         });
+    }
+
+    function createRow() {
+        var row = document.createElement('div');
+        row.classList.add('row');
+        return row;
+    }
+
+    function createCell(indexInBoard) {
+        var cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.indexInBoard = indexInBoard;
+
+        return cell;
+    }
+
+    function createBoard() {
+        var row;
+        var cell;
+        var x;
+        var y;
+        var fragment = document.createDocumentFragment();
+        for (x = 0; x < 10; x++) {
+            row = createRow();
+            fragment.appendChild(row);
+            for (y = 0; y < 10; y++) {
+                cell = createCell(x * 10 + y + 1);
+                row.appendChild(cell);
+            }
+        }
+
+        ELEMENTS.fieldElement.innerHTML = '';
+        ELEMENTS.fieldElement.appendChild(fragment);
+    }
+
+
+    function isCellElement(element) {
+        return element.classList.contains('cell');
+    }
+
+    function isGameFinished() {
+        return GAME.status === GEMA_STATE.ENDED;
+    }
+
+    function isFieldUsed(element) {
+        return element.classList.contains('x','o');
+    }
+
+    function showInWinnerField(text) {
+        ELEMENTS.mainGameStatusMessage.innerHTML = text;
+        return null;
+    }
+
+    function showWinnMessage(winner) {
+        showInWinnerField(winner === 'x'
+                ? 'Крестик победил'
+                : 'Нолик победил');
+    }
+
+    function clickOnField(event) {
+        var element = event.target;
+        var indexInBoard = element.indexInBoard;
+
+        if (GAME.nowSide === GAME.mySide) {
+
+
+            if (!isCellElement(element)
+                //|| isGameFinished() //TODO
+                || isFieldUsed(element)) {
+                return;
+            }
+
+            SEND.POST(
+                gameUrls.move,
+                { move: indexInBoard},
+                {'Game-ID': gameId, 'Player-ID': playerId},
+                function (data, statusCode) {
+                    if(statusCode !== 200) {
+
+                        var message = (data && data.message) || "Неизвестная ошибка";
+                        showInWinnerField(message);
+
+                        return;
+                    }
+
+                    element.classList.add(GAME.mySide);
+
+                    if(data && data.win) {
+                        showWinnMessage(data.win);
+                        GAME.status = GAME_STATE.ENDED;
+                        return;
+                    }
+
+                    GAME.nowSide = 'x' === GAME.nowSide ? 'o' : 'x';
+                    listenOfOpponent();
+                })
+
+        }
     }
 
     function initElements() {
         ELEMENTS = {
             startGame: document.querySelector('.startGame'),
             mainGame: document.querySelector('.mainGame'),
-            statusMessage: document.querySelector('.status-message'),
+            startGameStatusMessage: document.querySelector('.startGame .status-message'),
             createGame: document.querySelector('.createGame'),
+            fieldElement:  document.querySelector('.field'),
+            mainGameStatusMessage: document.querySelector('.mainGame .status-message'),
 
 
             remove: function remove(element) {
@@ -182,6 +364,10 @@
         }
 
         ELEMENTS.createGame.addEventListener('click', onCreateGame);
+
+        createBoard();
+
+        ELEMENTS.fieldElement.addEventListener('click', clickOnField);
     }
 
     function initApp() {
