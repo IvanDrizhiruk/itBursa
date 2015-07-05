ff01e0f170371401586052766c8af079f5fc05b7
IvanDrizhiruk <ivan.drizhiruk@gmail.com>
2015-07-05 22:11:16 +0300

    L6

--- L6/js/index.js
+++ L6/js/index.js
@@ -29,10 +29,73 @@ angular.module("jsbursa")
         "phone": "(328) 747-6780",
         "status": "removed"
       }];
+
+    $scope.students2 = [
+      {
+        "id": "5",
+        "name": "1",
+        "phone": "(466) 514-6617",
+        "status": "redcard"
+      }
+      ,
+      {
+        "id": "6",
+        "name": "2t",
+        "phone": "(314) 333-4959",
+        "status": "removed"
+      },
+      {
+        "id": "7",
+        "name": "3",
+        "phone": "(686) 869-6077",
+        "status": "removed"
+      }
+      ,
+      {
+        "id": "8",
+        "name": "4",
+        "phone": "(328) 747-6780",
+        "status": "removed"
+      }];
+
   }])
+
+
+
+//angular.module("jsbursa", [])
+
+function resort(items, stored) {
+  if (! stored) {
+      return;
+    }
+
+  var res = [];
+
+  stored.forEach(function (storedElm) {
+
+    var elm = _.find(items, function (fElm) {
+      return fElm.id === storedElm.id;
+    });
+
+    if (elm) {
+      res.push(elm);
+
+      _.remove(items, function (rElm) {
+        return rElm.id === storedElm.id;
+      });
+    }
+  });
+
+  items.forEach(function (addElm) {
+      res.push(addElm);
+  });
+
+  return res;
+}
 
 angular.module("jsbursa")
   .directive('draggableList', function() {
+
     return {
       scope: {
         items : "="
@@ -45,7 +108,72 @@ angular.module("jsbursa")
                 '</ul>',
       link: function (scope, element, attrs) {
 
-        element.find('ul').sortable({});
+        var elementId = attrs.id;
+
+        var currentUl = element.find('ul');
+
+        console.log("ISD elemtn " + elementId);
+        console.log(currentUl);
+
+
+
+        if (elementId) {
+          var saved = localStorage.getItem(elementId);
+          if (saved) {
+            console.log("ISD " + saved);
+            scope.items = resort(scope.items, JSON.parse(saved));
+          }
+        }
+
+
+        currentUl.sortable(
+          {
+            connectWith: "ul",
+
+            start: function(event, ui) {
+
+              var res = scope.items.splice(ui.item.index(), 1);
+
+              console.log("ISD id " + elementId);
+              console.log(scope.items);
+              console.log(res);
+
+              ui.item.data('data.item', {
+                index: ui.item.index(),
+                data: res[0]
+              });
+
+              if (elementId) {
+                localStorage.setItem(elementId, JSON.stringify(scope.items));
+              }
+            },
+            update: function(event, ui) {
+              console.log("ISD update");
+              if (this !== ui.item.parent()[0]) {
+                return;
+              }
+
+              var moveFrom = ui.item.data('data.item');
+              var moveTo = {index: ui.item.index()/*, status: extractContainerType(ui.item)*/};
+
+
+              var tmp = moveFrom.data;
+              scope.items.splice(moveTo.index, 0, tmp);
+
+              console.log("ISD end id " + elementId);
+              console.log(scope.items);
+              console.log(ui.item.index());
+
+
+              scope.$applyAsync();
+
+
+              if (elementId) {
+                localStorage.setItem(elementId, JSON.stringify(scope.items));
+              }
+            }
+          }
+        );
       }
     };
   });
